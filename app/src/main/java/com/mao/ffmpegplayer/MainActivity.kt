844e7818ef95367808d1a4e2997c5add57626839
mao <maoqitian068@163.com>
2020-12-01 16:12:10 +0800

    change cmakelists，but need arm64-v8a so

--- app/src/main/java/com/mao/ffmpegplayer/MainActivity.kt
+++ app/src/main/java/com/mao/ffmpegplayer/MainActivity.kt
@@ -2,10 +2,26 @@ package com.mao.ffmpegplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvText.text = getVersion()
+    }
+
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    private external fun getVersion(): String
+
+    companion object {
+        init {
+            System.loadLibrary("utils")
+            System.loadLibrary("avutil")
+        }
     }
 }
